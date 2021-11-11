5d8539b82e12bcb5d0298ac7ff904b29cdcf5a98
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2021-11-11 14:59:38 -0300

    Create Book entity

---
+++ src/main/java/com/example/demo/entities/Book.java
@@ -0,0 +1,103 @@
+package com.example.demo.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_book")
+public class Book implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+    private String name;
+    private String publisher;
+    
+    @Column(columnDefinition = "DATE")
+    private LocalDate publication_date; 
+    private String author;
+    
+    public Book() {
+    }
+
+	public Book(Long id, String name, String publisher, LocalDate publication_date, String author) {
+		this.id = id;
+		this.name = name;
+		this.publisher = publisher;
+		this.publication_date = publication_date;
+		this.author = author;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public LocalDate getPublication_date() {
+		return publication_date;
+	}
+
+	public void setPublication_date(LocalDate publication_date) {
+		this.publication_date = publication_date;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
