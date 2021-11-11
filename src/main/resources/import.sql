d0c97078eab5f593f12fbff0ca971c282720e88e
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2021-11-11 19:01:58 -0300

    Data Seeding

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,16 @@
 INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Grande Sertão: Veredas', 'Nova Fronteira', 2006, 'João Guimarães Rosa');
 INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Grande Sertão: Veredas', 'Nova Fronteira', 2015, 'João Guimarães Rosa');
 INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Grande Sertão: Veredas', 'Companhia das Letras', 2019, 'João Guimarães Rosa');
-INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Grande Sertão: Veredas', 'Companhia de Bolso', 2021, 'João Guimarães Rosa');
 INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Dom Casmurro', 'Penguin Companhia das Letras', 2016, 'Machado de Assis');
-INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Dom Casmurro', 'Panda Books', 2019, 'Machado de Assis');
 INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Dom Casmurro', 'Antofágica', 2020, 'Machado de Assis');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Sagarana', 'Nova Fronteira', 2015, 'João Guimarães Rosa');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Sagarana', 'Global', 2019, 'João Guimarães Rosa');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Quincas Borba', 'Penguin Companhia das Letras', 2012, 'Machado de Assis');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Quincas Borba', 'Antofágica', 2021, 'Machado de Assis');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Olhos d'água', 'Pallas', 2014, 'Conceição Evaristo');
 INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Vidas Secas', 'Record', 2014, 'Graciliano Ramos');
 INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Vidas Secas', 'Record', 2018, 'Graciliano Ramos');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Becos da Memória', 'Pallas', 2018, 'Conceição Evaristo');
 INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Quarto de Despejo', 'Ática', 2014, 'Carolina Maria de Jesus');
 INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Quarto de Despejo', 'Ática', 2021, 'Carolina Maria de Jesus');
 INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Cem Anos de Solidão', 'Record', 2014, 'Gabriel García Márquez');
