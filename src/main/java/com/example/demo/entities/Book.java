7b1767c451b69c6e1fe875e7600105744119f7ce
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2021-11-11 15:37:05 -0300

    Config H2 database

--- src/main/java/com/example/demo/entities/Book.java
+++ src/main/java/com/example/demo/entities/Book.java
@@ -1,9 +1,7 @@
 package com.example.demo.entities;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
